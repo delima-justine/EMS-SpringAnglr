5a200c99e0d7dbfece78b21eaa009495518c4882
delima-justine <delimajustine24@gmail.com>
2025-11-06 21:20:14 +0800

    backend(CourseRepo): add Repository annotation.

--- backend/ems/src/main/java/com/pupt/ems/ems/repository/CourseRepository.java
+++ backend/ems/src/main/java/com/pupt/ems/ems/repository/CourseRepository.java
@@ -1,8 +1,10 @@
 package com.pupt.ems.ems.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.pupt.ems.ems.model.Course;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
 }
