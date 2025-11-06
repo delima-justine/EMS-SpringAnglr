f3e6f214c4b42ce48b898280398a67ac6a2448e4
delima-justine <delimajustine24@gmail.com>
2025-11-06 22:48:51 +0800

    fix(model): match property of model to entity property in backend.

--- frontend/enrollment_management_system/src/models/ems.model.ts
+++ frontend/enrollment_management_system/src/models/ems.model.ts
@@ -1,10 +1,10 @@
 export type Course = {
-  course_id: string;
-  course_code: string;
-  course_title: string;
+  courseId: string;
+  courseCode: string;
+  courseTitle: string;
   units: number;
-  lecture_hours: number;
-  lab_hours: number;
-  dept_id: number;
-  is_deleted: boolean;
+  lectureHours: number;
+  labHours: number;
+  deptId: number;
+  isDeleted: boolean;
 }
