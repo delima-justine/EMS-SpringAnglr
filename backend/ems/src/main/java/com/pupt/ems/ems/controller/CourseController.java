6b17053fa8126dd5f375f45f1c7c2883608b6a79
delima-justine <delimajustine24@gmail.com>
2025-11-06 22:47:50 +0800

    backend: add cross origins to allow angular project to use API endpoint.

--- backend/ems/src/main/java/com/pupt/ems/ems/controller/CourseController.java
+++ backend/ems/src/main/java/com/pupt/ems/ems/controller/CourseController.java
@@ -3,12 +3,14 @@ package com.pupt.ems.ems.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pupt.ems.ems.model.Course;
 import com.pupt.ems.ems.repository.CourseRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CourseController {
