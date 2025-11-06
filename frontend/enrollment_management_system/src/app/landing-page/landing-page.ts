d2f2ff8300380241076f93b30fce483b92e1ca34
delima-justine <delimajustine24@gmail.com>
2025-11-06 17:34:06 +0800

    dependencies: add ng-icons(bootstrap)

--- frontend/enrollment_management_system/src/app/landing-page/landing-page.ts
+++ frontend/enrollment_management_system/src/app/landing-page/landing-page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { 
+        bootstrapJournalBookmarkFill,
+        bootstrapJournalBookmark,
+        bootstrapBuildingFill,
+        bootstrapList,
+        bootstrapClipboard2Fill,
+        bootstrapRobot,
+        bootstrapHousesFill,
+        bootstrapPeopleFill,
+        bootstrapCalendar2DateFill,
+      } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIcon],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
+  providers: [provideIcons({
+    bootstrapJournalBookmarkFill,
+    bootstrapJournalBookmark,
+    bootstrapBuildingFill,
+    bootstrapList,
+    bootstrapClipboard2Fill,
+    bootstrapRobot,
+    bootstrapHousesFill,
+    bootstrapPeopleFill,
+    bootstrapCalendar2DateFill
+  })]
 })
 export class LandingPage {
-
+  
 }
