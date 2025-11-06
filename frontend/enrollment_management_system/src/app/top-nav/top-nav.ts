5edef2256b0ddeb6788ea350f257eea0bb60f1fb
delima-justine <delimajustine24@gmail.com>
2025-11-06 19:41:34 +0800

    feat(top-nav): add function to top-nav dropdown to route to other pages.

--- frontend/enrollment_management_system/src/app/top-nav/top-nav.ts
+++ frontend/enrollment_management_system/src/app/top-nav/top-nav.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
   imports: [],
   templateUrl: './top-nav.html',
-  styleUrl: './top-nav.scss',
+  styleUrls: ['./top-nav.scss'],
 })
 export class TopNav {
+  constructor(private router: Router) {}
 
+  onSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    if (value) {
+      this.router.navigate([value]);
+    }
+  }
 }
