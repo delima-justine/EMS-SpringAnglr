84fb79423cc21cd0001395fb7a099fa75c3dce54
delima-justine <delimajustine24@gmail.com>
2025-11-06 19:49:05 +0800

    changes(course-prereqs): add content.

--- frontend/enrollment_management_system/src/app/course-prerequisites-page/course-prerequisites-page.ts
+++ frontend/enrollment_management_system/src/app/course-prerequisites-page/course-prerequisites-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TopNav } from "../top-nav/top-nav";
 
 @Component({
   selector: 'app-course-prerequisites-page',
-  imports: [],
+  imports: [TopNav],
   templateUrl: './course-prerequisites-page.html',
   styleUrl: './course-prerequisites-page.scss',
 })
