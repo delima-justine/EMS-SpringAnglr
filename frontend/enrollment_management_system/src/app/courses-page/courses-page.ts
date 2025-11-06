c52bbb05eece80ae07e007e909f9d128aa788073
delima-justine <delimajustine24@gmail.com>
2025-11-06 22:49:17 +0800

    feat: add courses from db.

--- frontend/enrollment_management_system/src/app/courses-page/courses-page.ts
+++ frontend/enrollment_management_system/src/app/courses-page/courses-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, signal } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
+import { Backend } from '../../service/backend';
+import { Course } from '../../models/ems.model';
 
 @Component({
   selector: 'app-courses-page',
@@ -7,6 +9,22 @@ import { TopNav } from "../top-nav/top-nav";
   templateUrl: './courses-page.html',
   styleUrl: './courses-page.scss',
 })
-export class CoursesPage {
+export class CoursesPage implements OnInit, AfterViewInit {
+  courses = signal(<Course[]>[]);
+  backendService = inject(Backend);
 
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  ngAfterViewInit(): void {
+    this.getCourses();
+  }
+
+  getCourses() {
+    this.backendService.getCourses()
+      .subscribe(coursesData => {
+        this.courses.set(coursesData);
+    });
+  }
 }
