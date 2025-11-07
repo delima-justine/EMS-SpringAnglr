d82f0f60e36b64f07af244a0e92da7065f7f916c
delima-justine <delimajustine24@gmail.com>
2025-11-07 19:33:23 +0800

    frontend: add instructors-page component.

---
+++ frontend/enrollment_management_system/src/app/instructors-page/instructors-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InstructorsPage } from './instructors-page';
+
+describe('InstructorsPage', () => {
+  let component: InstructorsPage;
+  let fixture: ComponentFixture<InstructorsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InstructorsPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InstructorsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/enrollment_management_system/src/app/instructors-page/instructors-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructors-page',
+  imports: [],
+  templateUrl: './instructors-page.html',
+  styleUrl: './instructors-page.scss',
+})
+export class InstructorsPage {
+
+}
