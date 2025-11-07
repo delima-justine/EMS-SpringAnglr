ae89ca3e216cd0eaef77f52434f9c354984f4fa6
delima-justine <delimajustine24@gmail.com>
2025-11-07 19:32:03 +0800

    frontend: add departments-page component.

---
+++ frontend/enrollment_management_system/src/app/departments-page/departments-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-departments-page',
+  imports: [],
+  templateUrl: './departments-page.html',
+  styleUrl: './departments-page.scss',
+})
+export class DepartmentsPage {
+
+}
