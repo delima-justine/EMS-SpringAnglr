f955da23a0a4cc8ffbb738ab30c5c8bd4c0a744b
delima-justine <delimajustine24@gmail.com>
2025-11-07 17:35:53 +0800

    fix: add @Embeddable annotation to CoursePrereqId Model.

--- backend/ems/src/main/java/com/pupt/ems/ems/model/CoursePrerequisiteId.java
+++ backend/ems/src/main/java/com/pupt/ems/ems/model/CoursePrerequisiteId.java
@@ -1,14 +1,15 @@
 package com.pupt.ems.ems.model;
 
 import java.io.Serializable;
-
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 
+@Embeddable
 public class CoursePrerequisiteId implements Serializable {
 
   @Column(name = "course_id")
   private Integer courseId;
-  
+
   @Column(name = "prereq_course_id")
   private Integer prereqCourseId;
 
