4c33765b9ddc7e54b7b0fdd41178e9933e3dd9a1
delima-justine <delimajustine24@gmail.com>
2025-11-07 19:35:24 +0800

    frontend: add sections-page component

---
+++ frontend/enrollment_management_system/src/app/sections-page/sections-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sections-page',
+  imports: [],
+  templateUrl: './sections-page.html',
+  styleUrl: './sections-page.scss',
+})
+export class SectionsPage {
+
+}
