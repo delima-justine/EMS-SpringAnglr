1b33625570c22dec5fa96236bda907aeb83d629e
delima-justine <delimajustine24@gmail.com>
2025-11-07 13:44:18 +0800

    backend: add deleteCourse API.

--- backend/ems/src/main/java/com/pupt/ems/ems/controller/CourseController.java
+++ backend/ems/src/main/java/com/pupt/ems/ems/controller/CourseController.java
@@ -1,10 +1,13 @@
 package com.pupt.ems.ems.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +68,17 @@ public class CourseController {
 
     Course updatedCourse = courseRepository.save(course);
     return ResponseEntity.ok(updatedCourse);
+  }
+
+  // Delete Course by ID
+  @DeleteMapping("/courses/{id}")
+  public ResponseEntity<Map<String, Boolean>> deleteCourse(@PathVariable Integer id) {
+    Course course = courseRepository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Course not found with id :" + id));
+
+      courseRepository.delete(course);
+      Map<String, Boolean> response = new HashMap<>();
+      response.put("deleted", Boolean.TRUE);
+      return ResponseEntity.ok(response);
   }
 }
