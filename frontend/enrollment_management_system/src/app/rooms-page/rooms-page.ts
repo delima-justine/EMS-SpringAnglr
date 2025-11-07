72ca49184013753484eb6ee26f4d342c04760165
delima-justine <delimajustine24@gmail.com>
2025-11-07 19:34:48 +0800

    frontend: add rooms-page component.

---
+++ frontend/enrollment_management_system/src/app/rooms-page/rooms-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-page',
+  imports: [],
+  templateUrl: './rooms-page.html',
+  styleUrl: './rooms-page.scss',
+})
+export class RoomsPage {
+
+}
