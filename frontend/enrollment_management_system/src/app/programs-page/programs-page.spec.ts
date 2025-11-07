a8f9162b1b6150888f732f259f7d734999bfacb9
delima-justine <delimajustine24@gmail.com>
2025-11-07 19:34:16 +0800

    frontend: add programs-page component.

---
+++ frontend/enrollment_management_system/src/app/programs-page/programs-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProgramsPage } from './programs-page';
+
+describe('ProgramsPage', () => {
+  let component: ProgramsPage;
+  let fixture: ComponentFixture<ProgramsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProgramsPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProgramsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/enrollment_management_system/src/app/programs-page/programs-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programs-page',
+  imports: [],
+  templateUrl: './programs-page.html',
+  styleUrl: './programs-page.scss',
+})
+export class ProgramsPage {
+
+}
