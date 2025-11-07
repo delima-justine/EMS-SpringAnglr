5f11c92874c63f7785f36d8a9f45c16dabe154ec
delima-justine <delimajustine24@gmail.com>
2025-11-08 07:11:23 +0800

    routes: add core component's routes

--- frontend/enrollment_management_system/src/app/app.routes.ts
+++ frontend/enrollment_management_system/src/app/app.routes.ts
@@ -14,5 +14,37 @@ export const routes: Routes = [
     path: 'course-prerequisites', loadComponent: () => import(
       './course-prerequisites-page/course-prerequisites-page')
         .then(m => m.CoursePrerequisitesPage)
+  },
+  { 
+    path: 'departments', loadComponent: () => import(
+    './departments-page/departments-page').then(m => m.DepartmentsPage)
+  },
+  { 
+    path: 'enrollments', loadComponent: () =>
+     import('./enrollments-page/enrollments-page').then(m => m.EnrollmentsPage),
+  },
+  {
+    path: 'instructors', loadComponent: () => import(
+      './instructors-page/instructors-page').then(m => m.InstructorsPage),
+  },
+  {
+    path: 'programs', loadComponent: () => import(
+      './programs-page/programs-page').then(m => m.ProgramsPage)
+  },
+  {
+    path: 'rooms', loadComponent: () => import(
+      './rooms-page/rooms-page').then(m => m.RoomsPage)
+  },
+  {
+    path: 'sections', loadComponent: () => import(
+      './sections-page/sections-page').then(m => m.SectionsPage)
+  },
+  {
+    path: 'students', loadComponent: () => import(
+      './students-page/students-page').then(m => m.StudentsPage)
+  },
+  {
+    path: 'terms', loadComponent: () => import(
+      './terms-page/terms-page').then(m => m.TermsPage)
   }
 ];
