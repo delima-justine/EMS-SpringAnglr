deee092c013552f3829400bd8d0232ea9d1a4ea0
delima-justine <delimajustine24@gmail.com>
2025-11-08 18:11:02 +0800

    frontend: add data to the table from service.

--- frontend/enrollment_management_system/src/app/departments-page/departments-page.ts
+++ frontend/enrollment_management_system/src/app/departments-page/departments-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
+import { Department } from '../../models/ems.model';
+import { DepartmentService } from '../../service/department.service';
 
 @Component({
   selector: 'app-departments-page',
@@ -7,6 +9,18 @@ import { TopNav } from "../top-nav/top-nav";
   templateUrl: './departments-page.html',
   styleUrl: './departments-page.scss',
 })
-export class DepartmentsPage {
+export class DepartmentsPage implements OnInit {
+  departments = signal(<Department[]>[]);
+  departmentService = inject(DepartmentService);
 
+  ngOnInit(): void {
+    this.getDepartments();
+  }
+
+  getDepartments() {
+    this.departmentService.getDepartments()
+      .subscribe(departmentsData => {
+        this.departments.set(departmentsData);
+      })
+  }
 }
