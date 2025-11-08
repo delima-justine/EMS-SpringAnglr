be0573281a5bb2181108652c00aefa890d22aff9
delima-justine <delimajustine24@gmail.com>
2025-11-08 19:56:45 +0800

    services: add DELETE and PUT method for department service.

--- frontend/enrollment_management_system/src/service/department.service.ts
+++ frontend/enrollment_management_system/src/service/department.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Department } from '../models/ems.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,13 @@ export class DepartmentService {
 
   addDepartment(department: Department) {
     return this.http.post<Department>(this.deptUrl, department);
+  }
+
+  updateDepartment(id: number, department: Department): Observable<Department> {
+    return this.http.put<Department>(`${this.deptUrl}/${id}`, department);
+  }
+
+  deleteDepartment(id: number): Observable<Department> {
+    return this.http.delete<Department>(`${this.deptUrl}/${id}`);
   }
 }
