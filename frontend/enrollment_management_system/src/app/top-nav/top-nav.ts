38f66551847b4121b9b6659cf16a4cbdb798ebc1
delima-justine <delimajustine24@gmail.com>
2025-11-08 13:44:55 +0800

    route: add route to default page to system name.

--- frontend/enrollment_management_system/src/app/top-nav/top-nav.ts
+++ frontend/enrollment_management_system/src/app/top-nav/top-nav.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top-nav.html',
   styleUrls: ['./top-nav.scss'],
 })
