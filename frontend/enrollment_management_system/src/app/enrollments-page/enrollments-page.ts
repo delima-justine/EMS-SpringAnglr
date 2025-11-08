140278e0a8179a2c038f4a245e7c751729836142
delima-justine <delimajustine24@gmail.com>
2025-11-08 22:01:05 +0800

    changes(enrollments): add data tp the enrollments table.

--- frontend/enrollment_management_system/src/app/enrollments-page/enrollments-page.ts
+++ frontend/enrollment_management_system/src/app/enrollments-page/enrollments-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
+import { Enrollment } from '../../models/ems.model';
+import { EnrollmentService } from '../../service/enrollment.service';
 
 @Component({
   selector: 'app-enrollments-page',
@@ -7,6 +9,20 @@ import { TopNav } from "../top-nav/top-nav";
   templateUrl: './enrollments-page.html',
   styleUrl: './enrollments-page.scss',
 })
-export class EnrollmentsPage {
+export class EnrollmentsPage implements OnInit {
+  enrollments = signal(<Enrollment[]>[]);
+  enrollmentService = inject(EnrollmentService);
 
+  ngOnInit() {
+    this.getEnrollments();
+  }
+
+  // Get Enrollments from the backend
+  getEnrollments() {
+    this.enrollmentService.getEnrollments()
+      .subscribe(enrollmentsData => {
+        this.enrollments.set(enrollmentsData);
+        console.log('Enrollments fetched:', enrollmentsData);
+      });
+  }
 }
