07c2de3517026aa3b206597d176429a482014725
delima-justine <delimajustine24@gmail.com>
2025-11-08 21:52:05 +0800

    service(enrollments): update service.

--- frontend/enrollment_management_system/src/service/enrollment.service.ts
+++ frontend/enrollment_management_system/src/service/enrollment.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Enrollment } from '../models/ems.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnrollmentService {
-  
+  private enrollmentUrl = "http://localhost:8080/api/enrollments";
+  http = inject(HttpClient);
+
+  getEnrollments() {
+    return this.http.get<Enrollment[]>(this.enrollmentUrl);
+  }
+
+  getEnrollmentById(id: number): Observable<Enrollment> {
+    return this.http.get<Enrollment>(`${this.enrollmentUrl}/${id}`);
+  }
+
+  addEnrollment(enrollment: Enrollment): Observable<Enrollment> {
+    return this.http.post<Enrollment>(this.enrollmentUrl, enrollment);
+  }
+
+  updateEnrollment(id: number, enrollment: Enrollment): Observable<Enrollment> {
+    return this.http.put<Enrollment>(`${this.enrollmentUrl}/${id}`, enrollment);
+  }
+
+  deleteEnrollment(id: number): Observable<Enrollment> {
+    return this.http.delete<Enrollment>(`${this.enrollmentUrl}/${id}`);
+  }
 }
