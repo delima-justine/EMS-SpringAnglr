459536f4edb4fc36ef4a4ff02fb3be8cc50ae8f8
delima-justine <delimajustine24@gmail.com>
2025-11-09 18:42:29 +0800

    service: add student service.

---
+++ frontend/enrollment_management_system/src/service/student.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Student } from '../models/ems.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentService {
+  studentUrl = 'http://localhost:8080/api/students';
+  http = inject(HttpClient);
+  
+  getStudent() {
+    return this.http.get<Student[]>(this.studentUrl);
+  }
+
+  getStudentById(studentId: number): Observable<Student> {
+    return this.http.get<Student>(`${this.studentUrl}/${studentId}`);
+  }
+
+  addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.studentUrl, student);
+  }
+
+  updateStudent(studentId: number, student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.studentUrl}/${studentId}`, student);
+  }
+
+  deleteStudent(studentId: number): Observable<Student> {
+    return this.http.delete<Student>(`${this.studentUrl}/${studentId}`);
+  }
+}
