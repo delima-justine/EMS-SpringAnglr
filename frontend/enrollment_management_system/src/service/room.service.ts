f3a420798d395f47c9771a816a6f6970186a53df
delima-justine <delimajustine24@gmail.com>
2025-11-09 20:31:09 +0800

    service: add room service.

---
+++ frontend/enrollment_management_system/src/service/room.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Room } from '../models/ems.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoomService {
+  roomUrl = 'http://localhost:8080/api/rooms';
+  http = inject(HttpClient);
+
+  getRooms() {
+    return this.http.get<Room[]>(this.roomUrl);
+  }
+
+  getRoomById(roomId: number): Observable<Room> {
+    return this.http.get<Room>(`${this.roomUrl}/${roomId}`);
+  }
+
+  addRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(this.roomUrl, room);
+  }
+
+  updateRoom(roomId: number, room: Room): Observable<Room> {
+    return this.http.put<Room>(`${this.roomUrl}/${roomId}`, room);
+  }
+
+  deleteRoom(roomId: number): Observable<Room> {
+    return this.http.delete<Room>(`${this.roomUrl}/${roomId}`);
+  }
+}
