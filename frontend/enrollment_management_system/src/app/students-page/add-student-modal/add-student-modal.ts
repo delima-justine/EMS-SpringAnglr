1b4684abfb2c1b521b5e06a61a9c72585e4532b3
delima-justine <delimajustine24@gmail.com>
2025-11-09 19:24:39 +0800

    feat(students): add "add student" feature.

---
+++ frontend/enrollment_management_system/src/app/students-page/add-student-modal/add-student-modal.ts
@@ -0,0 +1,62 @@
+import { Component, ElementRef, inject, output, ViewChild } from '@angular/core';
+import { 
+        ReactiveFormsModule,
+        FormControl,
+        FormGroup,
+        FormBuilder,
+        Validators,
+        Form
+      } from '@angular/forms';
+import { Modal } from 'bootstrap';
+import { StudentService } from '../../../service/student.service';
+import { Student } from '../../../models/ems.model';
+
+@Component({
+  selector: 'app-add-student-modal',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-student-modal.html',
+  styleUrl: './add-student-modal.scss',
+})
+export class AddStudentModal {
+  @ViewChild('addStudentModal') addStudentModal!: ElementRef;
+  @ViewChild('openStudentModalButton') openStudentModalButton!: ElementRef;
+  studentForm: FormGroup;
+  studentService = inject(StudentService);
+  response = output<Student>();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.studentForm = this.formBuilder.group({
+      studentNo: [''],
+      firstName: [''],
+      middleName: [''],
+      lastName: [''],
+      email: [''],
+      birthDate: [''],
+      gender: [''],
+      yearLevel: [''],
+      programId: [''],
+      isDeleted: [false]
+    });
+  }
+
+  openModal() {
+    const modal = new Modal(this.addStudentModal.nativeElement);
+    modal.show();
+  }
+
+  closeModal() {
+    const modal = Modal.getInstance(this.addStudentModal.nativeElement);
+    modal?.hide();
+
+    this.openStudentModalButton.nativeElement.focus();
+  }
+
+  addStudent() {
+    this.studentService.addStudent(this.studentForm.value)
+      .subscribe((createdStudent: Student) => {
+        this.response.emit(createdStudent);
+        this.studentForm.reset();
+        this.closeModal();
+    });
+  }
+}
--- frontend/enrollment_management_system/src/app/students-page/students-page.ts
+++ frontend/enrollment_management_system/src/app/students-page/students-page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
+import { Student } from '../../models/ems.model';
+import { StudentService } from '../../service/student.service';
+import { AddStudentModal } from "./add-student-modal/add-student-modal";
 
 @Component({
   selector: 'app-students-page',
-  imports: [TopNav],
+  imports: [TopNav, AddStudentModal],
   templateUrl: './students-page.html',
   styleUrl: './students-page.scss',
 })
-export class StudentsPage {
+export class StudentsPage implements OnInit {
+  students = signal(<Student[]>[]);
+  studentService = inject(StudentService);
 
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  // Get all students
+  getStudents() {
+    this.studentService.getStudent().subscribe(students => {
+      this.students.set(students);
+    });
+  }
+
+  // Soft delete student data
+  deleteStudent(studentId: number) {
+    this.studentService.deleteStudent(studentId).subscribe(() => {
+      this.getStudents();
+    });
+  }  
+
+  // Handle the event emitted from AddStudentModal component
+  onStudentAdded(newStudent: Student) {
+    this.students.update(student => [...student, newStudent]);
+  }
 }
