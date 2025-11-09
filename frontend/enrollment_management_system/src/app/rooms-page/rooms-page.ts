4e33c2eba08765aa6cbb357d2f9c84cedd247899
delima-justine <delimajustine24@gmail.com>
2025-11-09 20:31:47 +0800

    feat(rooms): add read and delete for rooms.

--- frontend/enrollment_management_system/src/app/rooms-page/rooms-page.ts
+++ frontend/enrollment_management_system/src/app/rooms-page/rooms-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
+import { Room } from '../../models/ems.model';
+import { RoomService } from '../../service/room.service';
 
 @Component({
   selector: 'app-rooms-page',
@@ -7,6 +9,25 @@ import { TopNav } from "../top-nav/top-nav";
   templateUrl: './rooms-page.html',
   styleUrl: './rooms-page.scss',
 })
-export class RoomsPage {
+export class RoomsPage implements OnInit {
+  rooms = signal(<Room[]>[]);
+  roomService = inject(RoomService);
 
+  ngOnInit(): void {
+    this.getRooms();
+  }
+
+  // Get Rooms
+  getRooms() {
+    this.roomService.getRooms().subscribe((data) => {
+      this.rooms.set(data);
+    });
+  }
+
+  // Soft Delete Room
+  deleteRooms(roomId: number) {
+    this.roomService.deleteRoom(roomId).subscribe(() => {
+      this.getRooms();
+    });
+  }
 }
