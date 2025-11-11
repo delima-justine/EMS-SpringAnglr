fcca7a713ce8b94e2f71f878de741ce6e3c8fe8d
delima-justine <delimajustine24@gmail.com>
2025-11-11 11:54:00 +0800

    feat(prerequisites): add sorting functionality for course prerequisites with ascending and descending options

--- frontend/enrollment_management_system/src/app/course-prerequisites-page/course-prerequisites-page.ts
+++ frontend/enrollment_management_system/src/app/course-prerequisites-page/course-prerequisites-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { TopNav } from "../top-nav/top-nav";
 import { CoursePrerequisite } from '../../models/ems.model';
 import { CoursePrereqService } from '../../service/course-prereq.service';
@@ -11,6 +11,7 @@ import { AddCpModal } from "./add-cp-modal/add-cp-modal";
   styleUrl: './course-prerequisites-page.scss',
 })
 export class CoursePrerequisitesPage implements OnInit {
+  @ViewChild('sortOrder') sortOrder!: ElementRef;
   coursePrerequisites = signal(<CoursePrerequisite[]>[]);
   coursePrereqService = inject(CoursePrereqService);
 
@@ -31,5 +32,24 @@ export class CoursePrerequisitesPage implements OnInit {
       console.log(coursePrerequisiteData);
       this.getCoursePrerequisites();
     })
+  }
+
+  sortCoursePrerequisites() {
+    const order = this.sortOrder.nativeElement.value;
+
+    switch(order) {
+      case 'asc':
+        this.coursePrereqService.sortCoursePrerequisitesAsc()
+          .subscribe(prereqData => {
+            this.coursePrerequisites.set(prereqData);
+        });
+        break;
+      case 'desc':
+        this.coursePrereqService.sortCoursePrerequisitesAsc()
+          .subscribe(prereqData => {
+            this.coursePrerequisites.set(prereqData.reverse());
+        });
+        break;
+    }
   }
 }
